ab2c66a25829cf338e985af7922d8e78d6348ba1
Bryce Ebsen <baebsen@ualr.edu>
2021-05-03 19:26:51 +0000

    finished cycle demo

--- cycle/assets/js/scripts.js
+++ cycle/assets/js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 $('.gallery').cycle({
-		fx: 'fade' // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+		fx: 'fade', // choose your transition type, ex: fade, scrollUp, shuffle, etc...
 	});
 
 });
